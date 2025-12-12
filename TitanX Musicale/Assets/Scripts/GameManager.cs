0b49ffdc98bc26401ddbc99b2473a5d59e696594
F25BA041 <f25ba041@chuo.ac.jp>
2025-12-12 14:49:20 +0900

    タイトル画面を作成 スカイボックスのデータが大きくてアップロードできないから別の方法で共有する

--- TitanX Musicale/Assets/Scripts/GameManager.cs
+++ TitanX Musicale/Assets/Scripts/GameManager.cs
@@ -2,4 +2,26 @@ using UnityEngine;
 
 public class GameManager
 {
+    static private GameManager instance;
+    static public GameManager Instance{
+        get {
+            if (instance == null) instance = new GameManager();
+            return instance;
+        }
+    }
+    private GameManager(){}
+    private SaveData _data;
+    public SaveData data{
+        get {
+            if (_data == null) _data = loadData();
+            return _data;
+        }
+    }
+    private SaveData loadData(){
+        return null;
+    }
+}
+
+public class SaveData{
+
 }
--- TitanX Musicale/Assets/Scripts/TitleSceneManager.cs
+++ TitanX Musicale/Assets/Scripts/TitleSceneManager.cs
@@ -2,15 +2,50 @@ using UnityEngine;
 
 public class TitleSceneManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private bool isOpening = true;
+    public GameObject openingTextList;
+    public Animator TitanAnimator;
+    public GameObject openingPanel;
+    public GameObject titlePanel;
+    public GameObject confirmWindow;
+    public GameObject continueButton;
+
     void Start()
     {
-        
+        if (GameManager.Instance.data == null) continueButton.SetActive(false);
+        openingPanel.SetActive(true);
+        titlePanel.SetActive(false);
+        confirmWindow.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (isOpening) {
+            isOpening = openingUpdate();
+            if (Input.GetMouseButtonDown(0)) {
+                endOpening();
+                isOpening = false;
+            }
+            return;
+        }
+    }
+
+    public float openingSpeed = 100;
+    private bool openingUpdate(){
+        Vector3 texPos = openingTextList.transform.position;
+        texPos.y += openingSpeed * Time.deltaTime;
+        openingTextList.transform.position = texPos;
+        Vector3 texScale = openingTextList.transform.localScale;
+        openingTextList.transform.localScale = texScale - Vector3.one * (openingSpeed * Time.deltaTime / 1000f);
+        if (texScale.x <= 0.3) {
+            endOpening();
+            return false;
+        }
+        return true;
+    }
+    private void endOpening(){
+        TitanAnimator.SetBool("opening", false);
+        openingPanel.SetActive(false);
+        titlePanel.SetActive(true);
     }
 }
